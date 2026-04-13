4d869a731a30ea35e36562f722e07cf6b05bc610
over-anna <annapati@icloud.com>
2026-04-13 10:40:04 -0500

    Update video-data.js

--- assets/data/video-data.js
+++ assets/data/video-data.js
@@ -1,12 +1,9 @@
 {
-  "videos": [
+  "shortcuts": [
     {
-      "title": "Pride and Prejudice",
-      "description": "Immerse yourself in the enchanting world of Pride and Prejudice by Jane Austen. Follow the spirited Elizabeth Bennet as she navigates the complexities of love, family, and societal expectations in this beloved classic that continues to captivate readers worldwide.",
-      "author": "Jane Austen",
-      "url": "https://www.youtube.com/watch?v=eVHu5-n69qQ",
-      "cover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvutMbOtMftj1v7m32zetGSgRDDtJmHhJVRQ&s",
-      "duration": "10:23:46",
-      "genre": ["romance", "fiction", "satire"]
+      "shortcut": "x",
+      "description": "x",
+      "author": "x",
+      "environment": "x"
     } ]
 }
